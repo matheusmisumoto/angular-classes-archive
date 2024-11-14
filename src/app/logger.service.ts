1b693c2e7323ed4111c31eb9c384e4faa1c8f9a5
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2024-11-13 21:19:31 -0300

    Refactor: change date format on LoggerService
    - Change how dates are logged in the LoggerService
    - Change style of log messages
    - Minor style changes

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -11,7 +11,16 @@ export class LoggerService {
   }
 
   add(message: string){
-    this.messages.push(message + ' at ' + new Date());
+    this.messages.push(message + ' at ' + new Date().toLocaleString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      timeZoneName: "short",
+      hour12: false
+    }));
   }
 
   clear(){
